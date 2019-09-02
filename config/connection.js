ba144c6408af39e8d0959db84f4926db40d21ca8
Annie Zoyin Lee <annie80113@hotmail.com>
2019-09-02 18:12:30 -0400

    ok

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("mate", "root", "root", {
+var sequelize = new Sequelize(process.env.JAWSDB_URL || "mate", "root", "root", {
   host: "localhost",
   port: 8889,
   dialect: "mysql",
