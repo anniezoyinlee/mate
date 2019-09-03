7e3e0a4408611a39cf762f40768dbde2cc64a391
Annie Zoyin Lee <annie80113@hotmail.com>
2019-09-03 19:12:41 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,11 @@
 // *********************************************************************************
 
 // Dependencies
+
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize(process.env.JAWSDB_URL || {"mate", "root", "root", {
-  host: "localhost",
-  port: 8889,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-}});
+var sequelize = new Sequelize(process.env.JAWSDB_URL)
 
 // Exports the connection for other files to use
 module.exports = sequelize;
